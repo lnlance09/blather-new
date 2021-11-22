c4f4e264d0d91331229fa00ad269e0d42a28ad8e
Lance Newman <lnewman@brightidea.com>
2021-11-22 11:41:48 -0500

    prod build

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -79,6 +79,6 @@
 
     <!-- React build JS -->
     <script src="/static/js/2.27f70a28.chunk.js"></script>
-    <script src="/static/js/main.98b0de8a.chunk.js"></script>
+    <script src="/static/js/main.7d72c613.chunk.js"></script>
     <script src="/static/js/runtime-main.4849eda3.js"></script>
 </html>
